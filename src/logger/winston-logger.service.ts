bfc2be6cc98d8ad1939f5973db3ea1c2040a6b4a
TarasYashchuk <tarasyashchuk089@gmail.com>
2024-10-04 12:49:06 +0300

    refactor: modified logger service

--- src/logger/winston-logger.service.ts
+++ src/logger/winston-logger.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { createLogger, format, transports } from 'winston';
+import * as winston from 'winston';
 
 @Injectable()
 export class WinstonLoggerService {
-  private readonly logger;
+  private logger: winston.Logger;
 
   constructor() {
-    this.logger = createLogger({
-      format: format.combine(format.timestamp(), format.json()),
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
       transports: [
-        new transports.File({ filename: 'logs/actions.log' }),
-        new transports.Console(),
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'combined.log' }),
       ],
     });
   }
 
-  logAction(action: string, userId: number, details: any) {
-    this.logger.info({ action, userId, details });
+  log(message: string, context?: any) {
+    this.logger.info(message, { context });
+  }
+
+  error(message: string, trace: string, context?: any) {
+    this.logger.error(message, { trace, context });
+  }
+
+  warn(message: string, context?: any) {
+    this.logger.warn(message, { context });
   }
 }
