bc8beec692c11487368d04e26c63296a018f9168
TarasYashchuk <tarasyashchuk089@gmail.com>
2024-10-04 14:52:42 +0300

    feat: added logging for followers service

--- src/followers/followers.module.ts
+++ src/followers/followers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FollowersService } from './followers.service';
 import { FollowersController } from './followers.controller';
 import { PrismaService } from 'src/prisma.service';
+import { WinstonLoggerService } from 'src/logger/winston-logger.service';
 
 @Module({
-  providers: [FollowersService, PrismaService],
+  providers: [FollowersService, PrismaService, WinstonLoggerService],
   controllers: [FollowersController],
 })
 export class FollowersModule {}
--- src/followers/followers.service.ts
+++ src/followers/followers.service.ts
@@ -3,80 +3,161 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import { WinstonLoggerService } from 'src/logger/winston-logger.service';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class FollowersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly logger: WinstonLoggerService,
+  ) {}
 
   async followUser(followerId: number, followingId: number) {
-    return this.prisma.follower.create({
-      data: {
-        followerId,
-        followingId,
-      },
-    });
-  }
+    const methodName = this.followUser.name;
+    this.logger.log(
+      `Start method ${methodName} executed by user with id ${followerId}`,
+    );
+    this.logger.log(
+      `User with id ${followerId} is attempting to follow user with id  ${followingId}`,
+    );
 
-  async unfollowUser(followerId: number, followingId: number) {
-    if (followerId === followingId) {
-      throw new BadRequestException('You cannot unfollow yourself');
-    }
-
-    const followRecord = await this.prisma.follower.findUnique({
-      where: {
-        followerId_followingId: {
+    try {
+      const follow = await this.prisma.follower.create({
+        data: {
           followerId,
           followingId,
         },
-      },
-    });
+      });
 
-    if (!followRecord) {
-      throw new NotFoundException(
-        `You are not following the user with ID ${followingId}`,
+      this.logger.log(
+        `User with id  ${followerId} successfully followed user with id ${followingId}`,
       );
+      await this.logger.logAction('FOLLOW_USER', followerId);
+      return follow;
+    } catch (error) {
+      this.logger.error(
+        `Error when user with id ${followerId} tried to follow user with id ${followingId}: ${error.message}`,
+      );
+      throw error;
     }
+  }
 
-    return this.prisma.follower.deleteMany({
-      where: {
-        followerId,
-        followingId,
-      },
-    });
+  async unfollowUser(followerId: number, followingId: number) {
+    const methodName = this.unfollowUser.name;
+    this.logger.log(
+      `Start method ${methodName} executed by user with id ${followerId}`,
+    );
+    this.logger.log(
+      `User with id ${followerId} is attempting to unfollow user with id ${followingId}`,
+    );
+
+    try {
+      if (followerId === followingId) {
+        throw new BadRequestException('You cannot unfollow yourself');
+      }
+
+      const followRecord = await this.prisma.follower.findUnique({
+        where: {
+          followerId_followingId: {
+            followerId,
+            followingId,
+          },
+        },
+      });
+
+      if (!followRecord) {
+        throw new NotFoundException(
+          `You are not following the user with ID with id ${followingId}`,
+        );
+      }
+
+      const unfollow = await this.prisma.follower.deleteMany({
+        where: {
+          followerId,
+          followingId,
+        },
+      });
+
+      this.logger.log(
+        `User with id ${followerId} successfully unfollowed user with id ${followingId}`,
+      );
+      await this.logger.logAction('UNFOLLOW_USER', followerId);
+      return unfollow;
+    } catch (error) {
+      this.logger.error(
+        `Error when user with id ${followerId} tried to unfollow user with id ${followingId}: ${error.message}`,
+      );
+      throw error;
+    }
   }
 
   async getFollowers(userId: number) {
-    const userExists = await this.prisma.user.findUnique({
-      where: { id: userId },
-    });
+    const methodName = this.getFollowers.name;
+    this.logger.log(
+      `Start method ${methodName} executed by user with id ${userId}`,
+    );
+    this.logger.log(`Fetching followers for user with id ${userId}`);
 
-    if (!userExists) {
-      throw new NotFoundException(`User with ID ${userId} not found`);
+    try {
+      const userExists = await this.prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (!userExists) {
+        throw new NotFoundException(`User with ID ${userId} not found`);
+      }
+
+      const followers = await this.prisma.follower.findMany({
+        where: { followingId: userId },
+        include: {
+          follower: { select: { id: true, username: true } },
+        },
+      });
+
+      this.logger.log(
+        `Followers for user with id ${userId} fetched successfully:`,
+      );
+      return followers;
+    } catch (error) {
+      this.logger.error(
+        `Error fetching followers for user with id ${userId}: ${error.message}`,
+      );
+      throw error;
     }
-
-    return this.prisma.follower.findMany({
-      where: { followingId: userId },
-      include: {
-        follower: { select: { id: true, username: true } },
-      },
-    });
   }
 
   async getFollowing(userId: number) {
-    const userExists = await this.prisma.user.findUnique({
-      where: { id: userId },
-    });
+    const methodName = this.getFollowing.name;
+    this.logger.log(
+      `Start method ${methodName} executed by user with id ${userId}`,
+    );
+    this.logger.log(`Fetching following users for user with id ${userId}`);
+    try {
+      const userExists = await this.prisma.user.findUnique({
+        where: { id: userId },
+      });
 
-    if (!userExists) {
-      throw new NotFoundException(`User with ID ${userId} not found`);
+      if (!userExists) {
+        throw new NotFoundException(`User with ID ${userId} not found`);
+      }
+
+      const following = await this.prisma.follower.findMany({
+        where: { followerId: userId },
+        include: {
+          following: { select: { id: true, username: true } },
+        },
+      });
+
+      this.logger.log(
+        `Following users for user with id ${userId} fetched successfully`,
+      );
+      return following;
+    } catch (error) {
+      this.logger.error(
+        `Error fetching following users for user with id ${userId}: ${error.message}`,
+      );
+      throw error;
     }
-
-    return this.prisma.follower.findMany({
-      where: { followerId: userId },
-      include: {
-        following: { select: { id: true, username: true } },
-      },
-    });
   }
 }
