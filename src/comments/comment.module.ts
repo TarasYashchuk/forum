13e242cb621ba90ee6dce7058687b9cfe8502a09
TarasYashchuk <tarasyashchuk089@gmail.com>
2024-10-04 14:27:50 +0300

    feat: added logging for comments service

--- src/comments/comment.module.ts
+++ src/comments/comment.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma.module';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { WinstonLoggerService } from 'src/logger/winston-logger.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [CommentService],
+  providers: [CommentService, WinstonLoggerService],
   controllers: [CommentController],
 })
 export class CommentsModule {}
