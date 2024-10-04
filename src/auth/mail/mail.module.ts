1c6dee8e121b7300f4af4ceaa5f7a9bf0b69b039
TarasYashchuk <tarasyashchuk089@gmail.com>
2024-10-04 15:16:05 +0300

    feat: added logger to mail service

--- src/auth/mail/mail.module.ts
+++ src/auth/mail/mail.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { WinstonLoggerService } from 'src/logger/winston-logger.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [MailService],
+  providers: [MailService, WinstonLoggerService, PrismaService],
   exports: [MailService],
 })
 export class MailModule {}
